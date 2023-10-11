4c184cdd243ccfec9b1947e19484a2d582210823
MK <manojmuduli9@gmail.com>
2023-10-11 09:28:38 +0530

    tabs UI done

--- src/components-core/Tabs/tabs.jsx
+++ src/components-core/Tabs/tabs.jsx
@@ -1,19 +1,20 @@
 import { twMerge as tm } from 'tailwind-merge'
 
-const Tab = ({ items, activeIndex }) => {
+const Tabs = ({ items, activeIndex, className, onChangeTab }) => {
     return (
-        <div className="mb-4 border-b border-gray-200">
-            <ul className="flex flex-wrap -mb-px text-sm font-medium text-center">
+        <div className={tm("mb-4 border-b border-gray-200", className)}>
+            <ul className="flex flex-wrap -mb-px text-sm font-medium text-center w-96">
                 {
                     items.map((eachItem, idx) => {
                         return (
-                            <li className="mr-2" key={eachItem.id || idx}>
+                            <li className="mr-2 flex-1" key={eachItem.id || idx}>
                                 <button className={
                                     tm(
                                         "inline-block p-4 rounded-t-lg hover:text-primary-600 hover:border-primary-300",
                                         activeIndex === idx && 'border-b-2 text-primary-600 border-primary-300'
                                     )
                                 }
+                                    onClick={() => { onChangeTab(eachItem.id || idx) }}
                                     type="button">
                                     {eachItem.name}
                                 </button>
@@ -27,4 +28,4 @@ const Tab = ({ items, activeIndex }) => {
     )
 }
 
-export default Tab;
+export default Tabs;
--- src/pages/chat.js
+++ src/pages/chat.js
@@ -1,4 +1,5 @@
-import Tab from "@/components-core/Tab/tab";
+import { useState } from 'react'
+import Tabs from "@/components-core/Tabs/tabs";
 import SearchSvg from "../assets/SearchSvg";
 import ThreeDotsSvg from "../assets/ThreeDots";
 import IconButton from "@/components-core/Button/IconButton";
@@ -6,6 +7,7 @@ import IconButton from "@/components-core/Button/IconButton";
 const TabList = [{ name: "Chat" }, { name: "Group" }, { name: "Status" }];
 
 function Chat() {
+  const [activeTabIndex, setActiveIndex] = useState(0);
   return (
     <>
       <section className="flex items-center px-6 py-4 gap-1 ">
@@ -19,7 +21,10 @@ function Chat() {
         </IconButton>
       </section>
       <section>
-        <Tab items={TabList} activeIndex={0} />
+        <Tabs className="flex justify-center " items={TabList} activeIndex={activeTabIndex} onChangeTab={setActiveIndex} />
+        <section>
+
+        </section>
       </section>
     </>
   );
